5481927fe6661718cf7099fa8f7a43026c0b9077
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2019-11-01 20:42:34 +0530

    Add files via upload

---
+++ EXP5_Bluetooth_Controlled_LED/Code.ino
@@ -0,0 +1,21 @@
+char data = 0;
+void setup()
+{
+pinMode(13,OUTPUT);
+Serial.begin(9600);
+}
+void loop()
+{
+if(Serial.available()>0)
+{
+data=Serial.read();
+if(data=='1')
+{
+digitalWrite(13,HIGH);
+}
+else if(data=='0')
+{
+digitalWrite(13,LOW);
+}
+}
+}
